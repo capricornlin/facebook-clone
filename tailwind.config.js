24cfe19d6f1ff3daa3a5275d73410f29e2e92df4
Caseylin <capricornlin1989@gmail.com>
2022-02-20 23:37:09 +0800

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   content: ["./src/**/*.html"],
 
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "main-span": "72px",
+      },
+      colors: {
+        "fb-bg": "#18191a",
+        "fb-header": "#242526",
+        "fb-input": "#3a3b3c",
+        "fb-popover": "#3e4042",
+        "fb-active": "#323436",
+        "fb-card": "#242526",
+        fb: "#2e89ff",
+      },
+    },
   },
   variants: {
     extend: {},
